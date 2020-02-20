720ed32609818d836903076004f7ea3e6126cc10
Luke Larson <luke.larson@gmail.com>
2020-02-19 22:36:25 -0800

    package depenedencies removed

--- Editor/Variable/SerializedVariableField.cs
+++ Editor/Variable/SerializedVariableField.cs
@@ -9,20 +9,14 @@ namespace PiRhoSoft.Variables.Editor
 	{
 		private const string _dataPropertyName = "_data";
 
-		private Label _label;
 		public VariableControl Control { get; private set; }
 
 		public SerializedVariableField(SerializedProperty property, VariableDefinition definition) : base(property.FindPropertyRelative(_dataPropertyName), Variable.Empty)
 		{
-			_label = new Label(property.displayName);
-			_label.AddToClassList(BaseField<int>.labelUssClassName);
 			Control = new VariableControl(definition);
 			Control.RegisterCallback<ChangeEvent<Variable>>(evt => Inject(evt.newValue));
 
-			Add(_label);
 			Add(Control);
-			style.flexDirection = FlexDirection.Row;
-
 			Extract();
 		}
 
