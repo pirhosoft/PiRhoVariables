edeb07cb704b1dc89bed1fee0438d5e38d7c3ab2
Luke Larson <luke.larson@gmail.com>
2020-02-26 23:14:08 -0800

    definition drawer fixes

--- Assets/PiRhoVariables/Editor/Variable/SerializedVariableField.cs
+++ Assets/PiRhoVariables/Editor/Variable/SerializedVariableField.cs
@@ -7,6 +7,10 @@ namespace PiRhoSoft.Variables.Editor
 {
 	public class SerializedVariableField : SerializedDataField<Variable>
 	{
+		public const string Stylesheet = "SerializedVariableStyle.uss";
+		public const string UssClassName = "pirho-serialized-variable";
+		public const string InputUssClassName = UssClassName + "__input";
+
 		private const string _dataPropertyName = "_data";
 
 		public VariableControl Control { get; private set; }
@@ -14,10 +18,14 @@ namespace PiRhoSoft.Variables.Editor
 		public SerializedVariableField(SerializedProperty property, VariableDefinition definition) : base(property.FindPropertyRelative(_dataPropertyName), Variable.Empty)
 		{
 			Control = new VariableControl(definition);
+			Control.AddToClassList(InputUssClassName);
 			Control.RegisterCallback<ChangeEvent<Variable>>(evt => Inject(evt.newValue));
 
+			AddToClassList(UssClassName);
 			Add(Control);
 			Extract();
+
+			this.AddStyleSheet(Stylesheet);
 		}
 
 		protected override void Load(SerializedDataReader reader, ref Variable value) => value = Variable.Load(reader);
